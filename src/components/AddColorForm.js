e2957fc97007594af08ce8e59dd0ca09273f991d
keroken <kenichi.shinagawa@gmail.com>
2021-10-21 00:02:19 +0900

    AddColorForm - refactor with controlled approach with state

--- src/components/AddColorForm.js
+++ src/components/AddColorForm.js
@@ -1,22 +1,31 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 
 export default function AddColorForm ({ onNewColor = f => f }) {
-  const textTitle = useRef();
-  const hexColor = useRef();
+  const [title, setTitle] = useState();
+  const [color, setColor] = useState();
 
   const submit = e => {
     e.preventDefault();
-    const title = textTitle.current.value;
-    const color = hexColor.current.value;
     onNewColor(title, color);
-    textTitle.current.value = "";
-    hexColor.current.value = "";
+    setTitle("");
+    setColor("");
   };
 
   return (
     <form onSubmit={submit}> 
-      <input ref={textTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        value={title}
+        onChange={event => setTitle(event.target.value)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={color}
+        onChange={event => setColor(event.target.value)}
+        type="color"
+        required
+      />
       <button>ADD</button>
     </form>
   );
