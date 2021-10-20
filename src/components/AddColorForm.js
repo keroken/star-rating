8f38cf4dcef9ad86712a7b917a2c7bb8cbd40684
keroken <kenichi.shinagawa@gmail.com>
2021-10-20 23:54:16 +0900

    create AddColorForm - imperative approach with ref

---
+++ src/components/AddColorForm.js
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react';
+
+export default function AddColorForm ({ onNewColor = f => f }) {
+  const textTitle = useRef();
+  const hexColor = useRef();
+
+  const submit = e => {
+    e.preventDefault();
+    const title = textTitle.current.value;
+    const color = hexColor.current.value;
+    onNewColor(title, color);
+    textTitle.current.value = "";
+    hexColor.current.value = "";
+  };
+
+  return (
+    <form onSubmit={submit}> 
+      <input ref={textTitle} type="text" placeholder="color title..." required />
+      <input ref={hexColor} type="color" required />
+      <button>ADD</button>
+    </form>
+  );
+}
