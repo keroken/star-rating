6ac61d4f73b459b1c62d211be3393667bcff97ef
keroken <kenichi.shinagawa@gmail.com>
2021-10-20 23:02:09 +0900

    add rating by click function

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { FaStar } from 'react-icons/fa';
+import { FaStar } from 'react-icons/fa';  
 
-const Star = ({ selected = false }) => 
-  (<FaStar color={selected ? "red" : "gray"} />);
+const Star = ({ selected = false, onClick }) => 
+  (<FaStar color={selected ? "red" : "gray"} onClick={onClick} />);
 
-const StarRating = ({ style = {}, totalStars = 5, selectedStars = 0 }) => {
+const StarRating = ({ style = {}, totalStars = 5, selectedStars = 0, onRate = f => f }) => {
   return (
     <div style={{ padding: "5px", ...style}}>
       {[...Array(totalStars)].map((n, i) => (
         <Star
           key={i}
           selected={selectedStars > i}
+          onClick={() => onRate(i + 1)}
+          style={{ fill: 'blue' }}
         />
       ))}
       <p>
